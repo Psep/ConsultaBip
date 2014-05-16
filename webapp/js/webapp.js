024a6816f63cf31c50922fbcb12e845e906839d2
psep <psep@debian.cl>
2014-05-16 00:28:51 -0400

    mod services

--- webapp/js/webapp.js
+++ webapp/js/webapp.js
@@ -115,7 +115,7 @@ $("#consultar").click(function() {
 			$.ajax({
 				type : 'GET',
 				dataType : 'json',
-				url : 'http://www.psep.cl/api/Bip.php?',
+				url : 'http://www.psep.cl/api/BipService.php?',
 				data : data,
 				success : successCallback,
 				error : errorCallback
